c3ee47a89cac3b40d33e361a8f396d24bb81d965
Drolfothesgnir <kyryl.yeletsky@gmail.com>
2023-03-20 12:01:13 +0200

    added new stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,85 +1,91 @@
-import queryString from "query-string";
-import { useState, useEffect, ChangeEvent } from "react";
-import "./App.css";
-import { Order } from "./utils/const";
-import useFilterApi from "./utils/filterApi/useFilterApi";
+import React from "react";
+import {
+  LaptopOutlined,
+  NotificationOutlined,
+  UserOutlined,
+  MenuOutlined
+} from "@ant-design/icons";
+import type { MenuProps } from "antd";
+import { Breadcrumb, Layout, Menu, theme, Row, Col, Typography } from "antd";
 
+const { Header, Content, Sider } = Layout;
+const {Title, Text} = Typography
 
-import instance from "./utils/http";
+const items1: MenuProps["items"] = ["1", "2", "3"].map((key) => ({
+  key,
+  label: `nav ${key}`,
+}));
 
-// TODO asObject() method for filtersApi; Operators api for filtersApi; remove toString() method from *Api
+const items2: MenuProps["items"] = [
+  UserOutlined,
+  LaptopOutlined,
+  NotificationOutlined,
+].map((icon, index) => {
+  const key = String(index + 1);
 
-type CategorySet = { [key: string]: boolean };
-interface Product {
-  id: number;
-  name: string;
-  price: number;
-  description: string;
-  imageUrl: string;
-  color: string;
-  categories: CategorySet;
-  brand: string;
-  availability: boolean;
-  rating: number;
-}
+  return {
+    key: `sub${key}`,
+    icon: React.createElement(icon),
+    label: `subnav ${key}`,
 
-function App() {
-  console.log(queryString.stringify({name: new RegExp('/\\d+')}));
-  
-  const [products, setProducts] = useState<Product[]>([]);
-  
-  const filterApi = useFilterApi({pagination: {perPage: 10, page: 1}})
+    children: new Array(4).fill(null).map((_, j) => {
+      const subKey = index * 4 + j + 1;
+      return {
+        key: subKey,
+        label: `option${subKey}`,
+      };
+    }),
+  };
+});
 
-  useEffect(function () {
-    
-    console.log(filterApi.getQueryString());
-    
-  }, [filterApi.value])
-
-  function getProducts() {
-    instance
-      .get(`/api/shop/products?${filterApi.getQueryString()}`)
-      .then((res) => {
-        setProducts(res.data);
-      })
-      .catch(console.log);
-  }
-
-  useEffect(() => {
-    getProducts();
-  }, [filterApi.value]);
-
-  function searchHandler(e: ChangeEvent) {
-    filterApi.search.set((e.target as HTMLInputElement).value)
-  }
+const App: React.FC = () => {
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
 
   return (
-    <div className="App">
-      <button disabled={(filterApi.value.pagination.page) < 2} onClick={filterApi.pagination.prev}>Prev page</button>
-      <input type="search" onChange={searchHandler} />
-      <button onClick={() => filterApi.sort.set("price", Order.DESC)}>
-        Sort Price
-      </button>
-      <button onClick={() => filterApi.sort.set("rating", Order.ASC)}>
-        Sort Rating
-      </button>
-      <button onClick={() => filterApi.pagination.unset("perPage", 0)}>match test</button>
-      <button onClick={filterApi.pagination.next}>Next page</button>
-      <ul>
-        {products.map((item) => {
-          return (
-            <li key={item.id}>
-              <img src={item.imageUrl} alt={item.name} />
-              <h3>{item.name}</h3>
-              <h4>$ {item.price}</h4>
-              <p>{item.description}</p>
-              <span>{item.color}</span>
-            </li>
-          );
-        })}
-      </ul>
-    </div>
+    <Layout>
+      <Header className="header">
+      <div className="logo">
+        {/* <img src={logo} alt="Logo" /> */}
+      </div>
+      <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
+        <Menu.Item key="1">Home</Menu.Item>
+        <Menu.Item key="2">Products</Menu.Item>
+        <Menu.Item key="3">Contact Us</Menu.Item>
+        <Menu.SubMenu key="4" icon={<MenuOutlined />} title="More">
+          <Menu.Item key="4.1">About Us</Menu.Item>
+          <Menu.Item key="4.2">FAQs</Menu.Item>
+        </Menu.SubMenu>
+      </Menu>
+    </Header>
+      <Layout>
+        <Sider
+        width="20%"
+          breakpoint="lg"
+          collapsedWidth="0"
+          onBreakpoint={(broken) => {
+            console.log(broken);
+          }}
+          onCollapse={(collapsed, type) => {
+            console.log(collapsed, type);
+          }}
+        ></Sider>
+        <Layout style={{ padding: "0 24px 24px" }}>
+          <Content
+            style={{
+              padding: 24,
+              margin: 0,
+              minHeight: 280,
+              background: colorBgContainer,
+            }}
+          >
+            Content
+          </Content>
+        </Layout>
+      </Layout>
+    </Layout>
   );
-}
+};
 
 export default App;
